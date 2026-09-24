32dab3021413e6f9153a94ad705b23992889600e
Vanyo <vanyolai@gmail.com>
2026-09-24 18:15:37 +0200

    Fix linked object date rendering

--- certificate/tpl/linkedobjectblock.tpl.php
+++ certificate/tpl/linkedobjectblock.tpl.php
@@ -5,7 +5,7 @@ if (empty($conf) || !is_object($conf)) {
 	exit(1);
 }
 
-global $linkedObjectBlock, $noMoreLinkedObjectBlockAfter;
+global $linkedObjectBlock, $noMoreLinkedObjectBlockAfter, $db;
 
 $langs->load('completioncertificate@completioncertificate');
 
@@ -21,7 +21,7 @@ foreach ($linkedObjectBlock as $key => $objectlink) {
 		<td><?php echo $langs->trans('CompletionCertificate'); ?></td>
 		<td><?php echo $objectlink->getNomUrl(1); ?></td>
 		<td></td>
-		<td class="center"><?php echo dol_print_date($objectlink->date_completion ? $objectlink->db->jdate($objectlink->date_completion) : 0, 'day'); ?></td>
+		<td class="center"><?php echo dol_print_date($objectlink->date_completion ? $db->jdate($objectlink->date_completion) : 0, 'day'); ?></td>
 		<td class="right"></td>
 		<td class="right"><?php echo $objectlink->getLibStatut(5); ?></td>
 		<td></td>
